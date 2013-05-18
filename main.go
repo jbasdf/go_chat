9fa7fe0379f2070c6e254e42f5d07d09f47e4335
Justin Ball <justinball@gmail.com>
2013-05-17 21:07:56 -0600

    Chat server

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+  "code.google.com/p/go.net/websocket"
+  "flag"
+  "log"
+  "net/http"
+  "text/template"
 )
 
-func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
+var addr = flag.String("addr", ":8080", "http service address")
+var homeTempl = template.Must(template.ParseFiles("home.html"))
+
+func homeHandler(c http.ResponseWriter, req *http.Request) {
+  homeTempl.Execute(c, req.Host)
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+func main() {
+  flag.Parse()
+  go h.run()
+  http.HandleFunc("/", homeHandler)
+  http.Handle("/ws", websocket.Handler(wsHandler))
+  if err := http.ListenAndServe(*addr, nil); err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
 }
