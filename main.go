8da3b6ad8212c876cb63c41c2a69c3020d91d942
Justin Ball <justinball@gmail.com>
2013-06-04 23:15:55 -0600

    Added eventsource

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
   "code.google.com/p/go.net/websocket"
+  eventsource "github.com/antage/eventsource/http"
   "flag"
   "log"
   "net/http"
   "text/template"
   "os"
+  "strconv"
+  "time"
+  "fmt"
 )
 
 //var addr = flag.String("addr", ":80", "http service address")
@@ -21,7 +25,24 @@ func main() {
   go h.run()
   http.HandleFunc("/", homeHandler)
   http.Handle("/ws", websocket.Handler(wsHandler))
+
+  es := eventsource.New(nil)
+  defer es.Close()
+  http.Handle("/events", es)
+  go func() {
+    fmt.Println("listening...")
+    id := 1
+    for {
+        es.SendMessage("tick", "tick-event", strconv.Itoa(id))
+        id++
+        time.Sleep(2 * time.Second)
+        fmt.Println("Sending id...")
+    }
+  }()
+
+
   if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
     log.Fatal("ListenAndServe:", err)
   }
+
 }
